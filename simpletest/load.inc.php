c58e3ddcfaba8f1b4a77aedf63a7256f20429044
lzhang <>
2007-10-05 16:14:44 +0000

    back to dirname

--- simpletest/load.inc.php
+++ simpletest/load.inc.php
@@ -2,6 +2,8 @@
 
 include_once('AS_TestCollector.class.php');
 
+define('APP_PATH', dirname(dirname(__FILE__)) . '/');
+
 // Include the simpletest library
 define('SIMPLETEST_PATH', dirname(__FILE__) . '/simpletest_1.0.1beta2/');
 include_once(SIMPLETEST_PATH . 'unit_tester.php');
