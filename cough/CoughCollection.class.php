4dad2f017a0114f64e798175c1299a72b2560578
awbush <>
2007-10-20 07:33:37 +0000

    Fixed parse error, can't call variable static functions, have to use call_user_func. Removed callMethodOnChildren completely, core Cough does not need it, and looping is easier for end-users.

--- cough/CoughCollection.class.php
+++ cough/CoughCollection.class.php
@@ -167,7 +167,7 @@ abstract class CoughCollection extends ArrayObject {
 		$result = $this->db->query($collectionSQL);
 		if ($result->numRows() > 0) {
 			while ($row = $result->getRow()) {
-				$this->add(new $elementClassName($row));
+				$this->add(call_user_func(array($elementClassName, 'constructByFields'), $row));
 			}
 		}
 		
@@ -184,31 +184,11 @@ abstract class CoughCollection extends ArrayObject {
 		$result = $this->db->query($sql);
 		if ($result->numRows() > 0) {
 			while ($row = $result->getRow()) {
-				$this->add($elementClassName::constructByFields($row));
+				$this->add(call_user_func(array($elementClassName, 'constructByFields'), $row));
 			}
 		}
 		$result->freeResult();
 	}
-
-	// 2007-10-18/AWB: Is this needed?
-	// /**
-	//  * Calls the specified method with the specified parameters on all the
-	//  * collected elements.
-	//  * 
-	//  * @param string $method - the method to call on all the collected elements
-	//  * @param mixed $params - the parameters to call the method with; either an array (if sending multiple parameters) or a single value
-	//  * @return void
-	//  * @author Anthony Bush
-	//  * @since 2007-10-16
-	//  **/
-	// public function callMethodOnChildren($method, $params = array()) {
-	// 	if (!is_array($params)) {
-	// 		$params = array($params);
-	// 	}
-	// 	foreach ($this as $element) {
-	// 		call_user_func_array(array($element, $method), $params);
-	// 	}
-	// }
 	
 	/**
 	 * Runs save on each collected element that is a CoughObject
