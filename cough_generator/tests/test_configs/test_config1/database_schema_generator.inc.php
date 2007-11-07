a484ad0ee29535b5f74424d9bb36ff8735813f7b
awbush <>
2007-11-07 04:55:33 +0000

    Configuration options have changed, need to specify which databases to include separately from configuration overrides for databases.

--- cough_generator/tests/test_configs/test_config1/database_schema_generator.inc.php
+++ cough_generator/tests/test_configs/test_config1/database_schema_generator.inc.php
@@ -3,6 +3,10 @@
 $config = array(
 	// REQUIRED CONFIG
 	
+	'database_settings' => array(
+		'include_databases_matching_regex' => '/^(test_cough_object)$/',
+	),
+	
 	// All databases will be scanned unless specified in the 'databases' parameter in the OPTIONAL CONFIG SECTION.
 	'dsn' => array(
 		'host' => 'localhost',
