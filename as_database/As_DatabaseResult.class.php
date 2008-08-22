673679124cb906bc31432bdd6c497cc93dc7fa69
Anthony Bush <anthony@anthonybush.com>
2008-08-21 22:36:27 -0500

    Added As_DatabaseResult::getRows() to pass the DAL tests I'm fixing.

--- as_database/As_DatabaseResult.class.php
+++ as_database/As_DatabaseResult.class.php
@@ -21,7 +21,16 @@ class As_DatabaseResult {
 	public function getRow() {
 		return mysql_fetch_assoc($this->result);
 	}
-
+	
+	public function getRows() {
+		$rows = array();
+		while ($row = mysql_fetch_assoc($this->result))
+		{
+			$rows[] = $row;
+		}
+		return $rows;
+	}
+	
 	public function getNumRows() {
 		return mysql_num_rows($this->result);
 	}
