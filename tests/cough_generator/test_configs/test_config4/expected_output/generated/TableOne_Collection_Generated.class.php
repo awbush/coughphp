527edc15c1272b6daba44470d7db3345715bdad9
Anthony Bush <anthony@anthonybush.com>
2008-08-28 22:45:39 -0500

    Added test case for lp bug #262475.

---
+++ tests/cough_generator/test_configs/test_config4/expected_output/generated/TableOne_Collection_Generated.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This is the base class for TableOne_Collection.
+ *
+ * @see TableOne_Collection, CoughCollection
+ **/
+abstract class TableOne_Collection_Generated extends CoughCollection {
+	protected $dbAlias = 'test_cough_object';
+	protected $dbName = 'test_cough_object';
+	protected $elementClassName = 'TableOne';
+}
+
+?>
