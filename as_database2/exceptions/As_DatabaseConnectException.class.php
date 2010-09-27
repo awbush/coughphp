dfff549ecd1a64cc50bf5887fc3c5c2914c675d9
Anthony Bush <anthony@anthonybush.com>
2010-09-27 00:10:49 -0500

    Use tabs, not spaces.

--- as_database2/exceptions/As_DatabaseConnectException.class.php
+++ as_database2/exceptions/As_DatabaseConnectException.class.php
@@ -1,45 +1,45 @@
 <?php
 class As_DatabaseConnectException extends As_DatabaseException 
 {
-   private $driver;
-   private $host;
-   private $port;
-   private $user;
-   private $error;
+	private $driver;
+	private $host;
+	private $port;
+	private $user;
+	private $error;
 
-   public function __construct($driver, $host, $port, $user, $error = NULL) 
-   {
-       parent::__construct('Unable to connect to ' . $driver . ' database server ' . $host . ':' . $port . ' as ' . $user . (isset($error) ? ': ' . $error : ''));
-       $this->host = $host;
-       $this->port = $port;
-       $this->user = $user;
-       $this->error = $error;
-       $this->driver = $driver;
-   }
+	public function __construct($driver, $host, $port, $user, $error = NULL) 
+	{
+		parent::__construct('Unable to connect to ' . $driver . ' database server ' . $host . ':' . $port . ' as ' . $user . (isset($error) ? ': ' . $error : ''));
+		$this->host = $host;
+		$this->port = $port;
+		$this->user = $user;
+		$this->error = $error;
+		$this->driver = $driver;
+	}
 
-   public function getDriver() 
-   {
-       return $this->driver;
-   }
+	public function getDriver() 
+	{
+		return $this->driver;
+	}
 
-   public function getHost() 
-   {
-       return $this->host;
-   }
+	public function getHost() 
+	{
+		return $this->host;
+	}
 
-   public function getPort() 
-   {
-       return $this->port;
-   }
+	public function getPort() 
+	{
+		return $this->port;
+	}
 
-      public function getUser() 
-   {
-       return $this->user;
-   }
+	public function getUser() 
+	{
+		return $this->user;
+	}
 
-   public function getError()
-   {
-   	   return $this->error;
-   }
+	public function getError()
+	{
+		return $this->error;
+	}
 }
 ?>
