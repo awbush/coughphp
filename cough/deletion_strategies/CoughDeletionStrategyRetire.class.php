47071d85b26cfb98529b898804bd9f55201912d5
Anthony Bush <anthony@anthonybush.com>
2013-11-08 21:50:23 -0600

    Update coughphp URL

--- cough/deletion_strategies/CoughDeletionStrategyRetire.class.php
+++ cough/deletion_strategies/CoughDeletionStrategyRetire.class.php
@@ -5,8 +5,8 @@
  *
  * @author Anthony Bush
  * @since 1.4
- * @see http://coughphp.com/blog/2008/09/04/modules-out-of-the-box-event-logging-for-coughphp/
- * @see http://coughphp.com/blog/2008/09/24/minutes-coughphp-developers-meeting/
+ * @see http://coughphp.anthonybush.com/blog/2008/09/04/modules-out-of-the-box-event-logging-for-coughphp/
+ * @see http://coughphp.anthonybush.com/blog/2008/09/24/minutes-coughphp-developers-meeting/
  **/
 class CoughDeletionStrategyRetire extends CoughDeletionStrategy
 {
--- docs/readme.txt
+++ docs/readme.txt
@@ -1,8 +1,8 @@
 This software is protected by the FreeBSD License (revised).  See license.txt for more info.
 
 For documentation, see the website:
-http://coughphp.com/
-http://coughphp.com/docs/
+http://coughphp.anthonybush.com/
+http://coughphp.anthonybush.com/docs/
 
 Don't forget about the IRC channel #coughphp on irc.freenode.net!
 
